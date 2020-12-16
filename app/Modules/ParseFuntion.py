6a3270ae0b1f95b88a6307f4fcdcb1017994456d
cober2019 <53579603+cober2019@users.noreply.github.com>
2020-12-16 10:31:00 -0800

    Add files via upload

--- app/Modules/ParseFuntion.py
+++ app/Modules/ParseFuntion.py
@@ -3,6 +3,7 @@
 import requests
 import warnings
 import json
+import string
 
 
 def custom_xpath(config, parent_key):
@@ -65,7 +66,7 @@ def get_capabilities(username, password, device, port, rest_obj):
         return response.status_code, modules, response.text
 
 
-def get_config_restconf(username, password, device, port, rest_obj, module='Module'):
+def get_config_restconf(username, password, device, port, rest_obj, module='Module', uri=None):
     """Requested Initial request for config via REST"""
 
     no_config = []
@@ -75,7 +76,9 @@ def get_config_restconf(username, password, device, port, rest_obj, module='Modu
     rest_obj.module = module
 
     warnings.filterwarnings('ignore', message='Unverified HTTPS request')
-    uri = f"https://{device}:{port}/restconf/data/{module}"
+    if uri is None:
+        uri = f"https://{device}:{port}/restconf/data/{module}"
+
     headers = {"Content-Type": 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
 
     # Request config. If wrong IP or access-denied == value return Access Denied to caller
@@ -91,11 +94,12 @@ def get_config_restconf(username, password, device, port, rest_obj, module='Modu
             format_response = validate_json(response)
 
             try:
+                print('yes')
                 lists = [k for k in config[parent_key].keys()]
                 return response.status_code, config, lists, format_response, leafs
             except AttributeError as error:
                 paths = custom_xpath(config, parent_key)
-                return response.status_code, config, paths[1], format_response, paths[2]
+                return response.status_code, config, paths[2], format_response, paths[1]
 
     except requests.exceptions.ConnectionError:
         return "Access Denied"
@@ -115,6 +119,10 @@ class ApiCalls:
         self.container = None
         self.lists = None
         self.leaf = None
+        self.cust_mod = None
+        self.attrb = None
+        self.attrb_2 = None
+        self.uri = None
         self.config_keys = None
 
     def request_container(self, username, password, device, container, port):
@@ -136,7 +144,6 @@ class ApiCalls:
 
         try:
             leafs = [k for k in config.get(f'{format_container}').keys()]
-            print(leafs)
             self.config_keys = leafs
         except AttributeError:
             leafs = ''
@@ -158,6 +165,7 @@ class ApiCalls:
         config = json.loads(response.text)
         format_res = json.dumps(response.json(), sort_keys=True, indent=4)
 
+
         try:
             leafs = [k for k in config.get(f'{lists}').keys()]
             self.config_keys = leafs
@@ -167,51 +175,72 @@ class ApiCalls:
 
         return format_res, leafs, self.config_keys
 
-    def request_custom_lists(self, username, password, device, lists, port):
+    def request_depth_one(self, username, password, device, lists, port, rest_obj):
         """Get request leaf from config"""
 
         self.lists = lists
+        self.cust_mod = self._module
+
         # Used to replace slashes in xpath filters. Usually used for interfaces
         if '/' in self._module and '/' in self.lists:
             conversion = lists.replace('/', '%2f')
             self.lists = self._module + '=' + conversion
-            uri = f"https://{device}:{port}/restconf/data/{self.lists}"
-        elif '/' in self._module and '/' not in self.lists and '-' not in self.lists:
-            uri = f"https://{device}:{port}/restconf/data/{self.module}={self.lists}"
+            self.uri = f"https://{device}:{port}/restconf/data/{self.lists}"
+        elif '/' in self._module and '/' not in self.lists and '-' not in self.lists and self.lists[0] not in string.ascii_letters:
+            self.uri = f"https://{device}:{port}/restconf/data/{self.module}={self.lists}"
+        elif ':' in self.lists:
+            self.uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.lists}"
         else:
             # Build uri based of button click which provides the leaf value
-            uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.lists}"
+            self.uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.lists}"
 
-        headers = {"Content-Type": 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
-        response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
-        config = json.loads(response.text)
-        get_keys = dict.fromkeys(json.loads(response.text))
-        parent_key = list(get_keys.keys())[0]
-        format_res = json.dumps(response.json(), sort_keys=True, indent=4)
+        get_config = get_config_restconf(username, password, device, port, rest_obj, module=self.cust_mod, uri=self.uri)
 
-        try:
-            leafs = [k for k in config.get(f'{parent_key}').keys()]
-            self.config_keys = leafs
-        except AttributeError as error:
-            leafs = ''
-            self.config_keys = leafs
+        return get_config
 
-        return format_res, leafs, self.config_keys
-
-    def request_leaf(self, username, password, device, leaf, port):
+    def request_depth_two(self, username, password, device, leaf, port, rest_obj):
         """Get request leaf from config"""
 
         self.leaf = leaf
-        warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
-        # Build uri based of button click which provides the leaf value
-        uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.container}/{self.lists}/{self.leaf}"
-        headers = {"Content-Type": 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
-        response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
+        # Used to replace slashes in xpath filters. Usually used for interfaces
+        if '/' in self._module and '/' in self.lists and '%' not in self.uri:
+            conversion = self.lists.replace('/', '%2f')
+            self.uri = f"https://{device}:{port}/restconf/data/{self.lists}"
+        elif '/' in self._module and '/' not in self.lists and '-' not in self.lists and self.lists[0] not in string.ascii_letters and '=' not in self.uri:
+            self.uri = f"https://{device}:{port}/restconf/data/{self.module}={self.lists}"
+        elif ':' in self.lists:
+            self.uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.lists}={self.leaf}"
+        elif '=' in self.uri:
+            self.uri = f"{self.uri}?fields={self.leaf}"
+        else:
+            # Build uri based of button click which provides the leaf value
+            conversion = self.leaf.replace('/', '%2f')
+            if '%' not in conversion:
+                self.uri = f"https://{device}:{port}/restconf/data/{self.module}{self.lists}={self.leaf}"
+            else:
+                self.uri = f"https://{device}:{port}/restconf/data/{self.module}{self.lists}={conversion}"
 
-        format_res = json.dumps(response.json(), sort_keys=True, indent=4)
+        get_config = get_config_restconf(username, password, device, port, rest_obj, module=self.cust_mod, uri=self.uri)
 
-        return format_res, self.config_keys
+        return get_config
+
+    def request_depth_three(self, username, password, device, attrb, port, rest_obj):
+        """Get request leaf from config"""
+
+        self.attrb = attrb
+        self.uri = f"{self.uri}?fields={self.attrb}"
+        get_config = get_config_restconf(username, password, device, port, rest_obj, module=self.cust_mod, uri=self.uri)
+        return get_config
+
+    def request_depth_four(self, username, password, device, attrb, port, rest_obj):
+        """Get request leaf from config"""
+
+        self.attrb_2 = attrb
+        self.uri = f"https://{device}:{port}/restconf/data/{self.module}/{self.lists}={self.leaf}?fields={self.attrb}/{self.attrb_2}"
+        get_config = get_config_restconf(username, password, device, port, rest_obj, uri=self.uri)
+
+        return get_config
 
     def reset(self):
         self.config_keys = None
